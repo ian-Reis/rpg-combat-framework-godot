fe864a8cf6a8ce59fcb1b3b28f52f416285195ef
Erickian2007 <erickian2007@gmail.com>
2026-06-08 18:00:37 -0300

    Update animation parameter paths to include StateMachine for consistency

--- source/constants/AnimationParams.cs
+++ source/constants/AnimationParams.cs
@@ -2,10 +2,10 @@ namespace Constants;
 
 public static class AnimationParams
 {
-    public const string IsIdle     = "parameters/conditions/IsIdle";
-    public const string IsWalk     = "parameters/conditions/IsWalk";
-    public const string IsRun      = "parameters/conditions/IsRun";
-    public const string IsJump     = "parameters/conditions/IsJump";
-    public const string IsWalkJump = "parameters/conditions/IsWalkJump";
-    public const string IsTravel   = "parameters/conditions/IsTravel";
+    public const string IsIdle     = "parameters/StateMachine/conditions/IsIdle";
+    public const string IsWalk     = "parameters/StateMachine/conditions/IsWalk";
+    public const string IsRun      = "parameters/StateMachine/conditions/IsRun";
+    public const string IsJump     = "parameters/StateMachine/conditions/IsJump";
+    public const string IsWalkJump = "parameters/StateMachine/conditions/IsWalkJump";
+    public const string IsTravel   = "parameters/StateMachine/conditions/IsTravel";
 }
