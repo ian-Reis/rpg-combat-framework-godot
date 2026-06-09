003a486532b09e0fd2ea9359a8132905dc1a9ce2
ian-Reis <erickian2007@gmail.com>
2026-06-09 18:58:58 -0300

    comment out all methods in PlayerStateTravel

--- source/resources/states/player/PlayerStateTravel.cs
+++ source/resources/states/player/PlayerStateTravel.cs
@@ -8,25 +8,25 @@ namespace Resources.states;
 [GlobalClass]
 public partial class PlayerStateTravel : PlayerState
 {
-    public override void Enter(ISystemLogicContext context)
-    {
-        AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsTravel, true);
-    }
+    // public override void Enter(ISystemLogicContext context)
+    // {
+    //     AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsTravel, true);
+    // }
 
-    public override void PhysicsUpdate(ISystemLogicContext context, float delta)
-    {
-        if (context?.Pawn is not CharacterBody3D pawn) return;
+    // public override void PhysicsUpdate(ISystemLogicContext context, float delta)
+    // {
+    //     if (context?.Pawn is not CharacterBody3D pawn) return;
 
-        JumpHelper.ApplyJump(context);
-        JumpHelper.JumpTravel(context);
-        pawn.MoveAndSlide();
+    //     JumpHelper.ApplyJump(context);
+    //     JumpHelper.JumpTravel(context);
+    //     pawn.MoveAndSlide();
 
-        if (PlanetsHelper.ChangedPlanet(context))
-            StateMachineHelper.ChangeState(context, PlayerStateNames.Idle);
-    }
+    //     if (PlanetsHelper.ChangedPlanet(context))
+    //         StateMachineHelper.ChangeState(context, PlayerStateNames.Idle);
+    // }
 
-    public override void Exit(ISystemLogicContext context)
-    {
-        AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsTravel, false);
-    }
+    // public override void Exit(ISystemLogicContext context)
+    // {
+    //     AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsTravel, false);
+    // }
 }
