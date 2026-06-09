7e14be2ea43b79267d4aa13ec831af73ec5615e0
ian-Reis <erickian2007@gmail.com>
2026-06-09 18:58:49 -0300

    comment out all methods in PlayerStateJump

--- source/resources/states/player/PlayerStateJump.cs
+++ source/resources/states/player/PlayerStateJump.cs
@@ -8,24 +8,24 @@ namespace Resources.states;
 [GlobalClass]
 public partial class PlayerStateJump : PlayerState
 {
-    public override void Enter(ISystemLogicContext context)
-    {
-        AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsJump, true);
-    }
+    // public override void Enter(ISystemLogicContext context)
+    // {
+    //     AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsJump, true);
+    // }
 
-    public override void PhysicsUpdate(ISystemLogicContext context, float delta)
-    {
-        if (context?.Pawn is not CharacterBody3D pawn) return;
+    // public override void PhysicsUpdate(ISystemLogicContext context, float delta)
+    // {
+    //     if (context?.Pawn is not CharacterBody3D pawn) return;
 
-        MovementHelper.ApplyMovement(context, delta, ignoreAirControl: false, canRun: false);
-        pawn.MoveAndSlide();
+    //     MovementHelper.ApplyMovement(context, delta, ignoreAirControl: false, canRun: false);
+    //     pawn.MoveAndSlide();
 
-        if (pawn.IsOnFloor())
-            StateMachineHelper.ChangeState(context, PlayerStateNames.Idle);
-    }
+    //     if (pawn.IsOnFloor())
+    //         StateMachineHelper.ChangeState(context, PlayerStateNames.Idle);
+    // }
 
-    public override void Exit(ISystemLogicContext context)
-    {
-        AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsJump, false);
-    }
+    // public override void Exit(ISystemLogicContext context)
+    // {
+    //     AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsJump, false);
+    // }
 }
