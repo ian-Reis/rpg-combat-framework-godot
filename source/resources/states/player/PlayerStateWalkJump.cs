6ef724dd2cf4af5f2c668414ba4d4e4b7f47ff7d
ian-Reis <erickian2007@gmail.com>
2026-06-09 18:59:07 -0300

    comment out all methods in PlayerStateWalkJump

--- source/resources/states/player/PlayerStateWalkJump.cs
+++ source/resources/states/player/PlayerStateWalkJump.cs
@@ -8,35 +8,35 @@ namespace Resources.states;
 [GlobalClass]
 public partial class PlayerStateWalkJump : PlayerState
 {
-    public override void Enter(ISystemLogicContext context)
-    {
-        AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsWalkJump, true);
-    }
+    // public override void Enter(ISystemLogicContext context)
+    // {
+    //     AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsWalkJump, true);
+    // }
 
-    public override void PhysicsUpdate(ISystemLogicContext context, float delta)
-    {
-        if (context?.Pawn is not CharacterBody3D pawn) return;
+    // public override void PhysicsUpdate(ISystemLogicContext context, float delta)
+    // {
+    //     if (context?.Pawn is not CharacterBody3D pawn) return;
 
-        MovementHelper.ApplyMovement(context, delta);
-        pawn.MoveAndSlide();
+    //     MovementHelper.ApplyMovement(context, delta);
+    //     pawn.MoveAndSlide();
 
-        if (pawn.IsOnFloor())
-        {
-            var inputDir = MovementHelper.GetInputDirection();
-            bool isMoving = inputDir.Length() > 0f;
-            bool isRunning = Input.IsActionPressed("run");
+    //     if (pawn.IsOnFloor())
+    //     {
+    //         var inputDir = MovementHelper.GetInputDirection();
+    //         bool isMoving = inputDir.Length() > 0f;
+    //         bool isRunning = Input.IsActionPressed("run");
 
-            if (!isMoving)
-                StateMachineHelper.ChangeState(context, PlayerStateNames.Idle);
-            else if (isRunning)
-                StateMachineHelper.ChangeState(context, PlayerStateNames.Run);
-            else
-                StateMachineHelper.ChangeState(context, PlayerStateNames.Walk);
-        }
-    }
+    //         if (!isMoving)
+    //             StateMachineHelper.ChangeState(context, PlayerStateNames.Idle);
+    //         else if (isRunning)
+    //             StateMachineHelper.ChangeState(context, PlayerStateNames.Run);
+    //         else
+    //             StateMachineHelper.ChangeState(context, PlayerStateNames.Walk);
+    //     }
+    // }
 
-    public override void Exit(ISystemLogicContext context)
-    {
-        AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsWalkJump, false);
-    }
+    // public override void Exit(ISystemLogicContext context)
+    // {
+    //     AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsWalkJump, false);
+    // }
 }
