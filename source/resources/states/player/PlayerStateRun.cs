ee38fcfe17a7e66516bec35d6bf82da13e5bb86c
ian-Reis <erickian2007@gmail.com>
2026-06-09 18:58:53 -0300

    update PlayerStateRun to StateMachineComponent pattern

--- source/resources/states/player/PlayerStateRun.cs
+++ source/resources/states/player/PlayerStateRun.cs
@@ -2,35 +2,40 @@ using Godot;
 using Helpers;
 using Interfaces;
 using Constants;
+using Components;
 
 namespace Resources.states;
 
 [GlobalClass]
 public partial class PlayerStateRun : PlayerState
 {
-    public override void Enter(ISystemLogicContext context)
+    public override void Enter(StateMachineComponent stateMachineComponent)
     {
-        AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsRun, true);
+        AnimationTreeHelper.SetTreeCondition(stateMachineComponent, AnimationParams.IsRun, true);
     }
 
-    public override void PhysicsUpdate(ISystemLogicContext context, float delta)
+    public override void PhysicsUpdate(StateMachineComponent stateMachineComponent  , float delta)
     {
-        if (context?.Pawn is not CharacterBody3D pawn) return;
 
-        JumpHelper.ApplyJump(context);
-        MovementHelper.ApplyMovement(context, delta);
+       if (stateMachineComponent?.systemLogicContext is not ISystemLogicContext logicContext) return;
+
+        var pawn = logicContext.Pawn as CharacterBody3D;
+        if (pawn == null) return;
+
+        JumpHelper.ApplyJump(logicContext);
+        MovementHelper.ApplyMovement(logicContext, delta);
         pawn.MoveAndSlide();
 
         var inputDir = MovementHelper.GetInputDirection();
         bool isMoving  = inputDir.Length() > 0f;
         bool isRunning = Input.IsActionPressed("run");
 
-        if (!isMoving)  { StateMachineHelper.ChangeState(context, PlayerStateNames.Idle); return; }
-        if (!isRunning) { StateMachineHelper.ChangeState(context, PlayerStateNames.Walk); return; }
+        if (!isMoving)  { StateMachineHelper.ChangeState(stateMachineComponent, PlayerStateNames.Idle); return; }
+        if (!isRunning) { StateMachineHelper.ChangeState(stateMachineComponent, PlayerStateNames.Walk); return; }
     }
 
-    public override void Exit(ISystemLogicContext context)
+    public override void Exit(StateMachineComponent stateMachineComponent)
     {
-        AnimationTreeHelper.SetTreeCondition(context, AnimationParams.IsRun, false);
+        AnimationTreeHelper.SetTreeCondition(stateMachineComponent, AnimationParams.IsRun, false);
     }
 }
