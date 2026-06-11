427d4ce0cc8fcbc0b42031e33fedf405c2744bf4
ian-Reis <erickian2007@gmail.com>
2026-06-10 22:19:25 -0300

    extend ISystemLogicContext with IHasAnimationStateMachineComponent

--- source/interfaces/ISystemLogicContext.cs
+++ source/interfaces/ISystemLogicContext.cs
@@ -4,7 +4,7 @@ using Data;
 // ReSharper disable once CheckNamespace
 namespace Interfaces;
 
-public interface ISystemLogicContext : IComponentOwner, IHasStateMachineComponent
+public interface ISystemLogicContext : IComponentOwner, IHasStateMachineComponent, IHasAnimationStateMachineComponent
 {
     Node3D Pawn { get; }
     CharacterStats Stats { get; }
