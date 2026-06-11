dc0cfaeb5b336d0904d25060acb7d741993a1977
ian-Reis <erickian2007@gmail.com>
2026-06-11 14:45:52 -0300

    update animation params path from locomotion to logic

--- source/constants/AnimationParams.cs
+++ source/constants/AnimationParams.cs
@@ -2,10 +2,10 @@ namespace Constants;
 
 public static class AnimationParams
 {
-    public const string IsIdle     = "parameters/locomotion/conditions/IsIdle";
-    public const string IsWalk     = "parameters/locomotion/conditions/IsWalk";
-    public const string IsRun      = "parameters/locomotion/conditions/IsRun";
-    public const string IsJump     = "parameters/locomotion/conditions/IsJump";
-    public const string IsWalkJump = "parameters/locomotion/conditions/IsWalkJump";
-    public const string IsTravel   = "parameters/locomotion/conditions/IsTravel";
+    public const string IsIdle     = "parameters/logic/conditions/IsIdle";
+    public const string IsWalk     = "parameters/logic/conditions/IsWalk";
+    public const string IsRun      = "parameters/logic/conditions/IsRun";
+    public const string IsJump     = "parameters/logic/conditions/IsJump";
+    public const string IsWalkJump = "parameters/logic/conditions/IsWalkJump";
+    public const string IsTravel   = "parameters/logic/conditions/IsTravel";
 }
