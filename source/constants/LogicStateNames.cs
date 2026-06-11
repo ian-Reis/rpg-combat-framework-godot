7db008bb62bfa1ab67e6f4543bc193b70d9593e5
ian-Reis <erickian2007@gmail.com>
2026-06-11 14:57:12 -0300

    add LogicStateNames constants

---
+++ source/constants/LogicStateNames.cs
@@ -0,0 +1,11 @@
+namespace Constants;
+
+public static class LogicStateNames
+{
+    public const string Idle     = "idle";
+    public const string Walk     = "walk";
+    public const string Run      = "run";
+    public const string Jump     = "jump";
+    public const string WalkJump = "walk_jump";
+    public const string Travel   = "travel";
+}
