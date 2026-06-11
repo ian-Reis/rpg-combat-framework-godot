f2c36106cf39c99d703fa71c9072404062d1d063
ian-Reis <erickian2007@gmail.com>
2026-06-11 15:53:37 -0300

    add acceleration/deceleration stats for ground and air

--- source/data/CharacterStats.cs
+++ source/data/CharacterStats.cs
@@ -9,12 +9,20 @@ public partial class CharacterStats : Resource
     [Export] public float WalkSpeed = 3f;
     [Export] public float RunSpeed  = 6f;
 
+    [ExportGroup("Movement/Ground")]
+    [Export] public float Acceleration  = 20f;
+    [Export] public float Deceleration  = 25f;
+
+    [ExportGroup("Movement/Air")]
+    [Export] public float AirAcceleration = 8f;
+    [Export] public float AirDeceleration = 4f;
+
     [ExportGroup("Jump")]
     [Export] public float JumpForce     = 8f;
     [Export] public float JumpHoldTime  = 0.2f;
     [Export] public float CutJumpFactor = 0.5f;
 
     [ExportGroup("Physics")]
-    [Export] public float Gravity       = 9.8f;
-    [Export] public float AirResistance = 0f;
+    [Export] public float Gravity = 9.8f;
+    
 }
