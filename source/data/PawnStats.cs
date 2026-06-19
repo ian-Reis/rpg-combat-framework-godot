42c1775d926c3a735b6dee5e248c442efdf25e5f
Erickian2007 <erickian2007@gmail.com>
2026-06-19 15:04:56 -0300

    add PawnStats Resource (replaces CharacterStats)

---
+++ source/data/PawnStats.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+namespace Data;
+
+[GlobalClass]
+public partial class PawnStats : Resource
+{
+    [ExportGroup("Movement")]
+    [Export] public float WalkSpeed = 3f;
+    [Export] public float RunSpeed  = 6f;
+
+    [ExportGroup("Movement/Ground")]
+    [Export] public float Acceleration  = 20f;
+    [Export] public float Deceleration  = 25f;
+
+    [ExportGroup("Movement/Air")]
+    [Export] public float AirAcceleration = 8f;
+    [Export] public float AirDeceleration = 4f;
+
+    [ExportGroup("Jump")]
+    [Export] public float JumpForce     = 8f;
+    [Export] public float JumpHoldTime  = 0.2f;
+    [Export] public float CutJumpFactor = 0.5f;
+
+    [ExportGroup("Health")]
+    [Export] public float MaxHealth = 100f;
+
+    [ExportGroup("Physics")]
+    [Export] public float Gravity = 9.8f;
+    
+}
