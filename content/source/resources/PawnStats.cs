b9efd76fe03980c4e597f5a9111155eaf91449ce
Erickian2007 <erickian2007@gmail.com>
2026-06-21 17:50:03 -0300

    Refactor PawnStats with export groups and auto-properties

--- content/source/resources/PawnStats.cs
+++ content/source/resources/PawnStats.cs
@@ -1,11 +1,19 @@
 using Godot;
-using System;
 
 namespace RPGFramework.Resources;
 
 [GlobalClass]
 public partial class PawnStats : Resource
 {
-    [Export]
-    public float Speed = 5.0f;
+    [ExportGroup("Movement")]
+    [Export] public float Speed { get; set; } = 5.0f;
+    [Export] public float Acceleration { get; set; } = 15.0f;
+    [Export] public float Friction { get; set; } = 10.0f;
+
+    [ExportGroup("Jump & Gravity")]
+    [Export] public float JumpForce { get; set; } = 5.0f;
+    [Export] public float GravityScale { get; set; } = 1.0f;
+
+    [ExportGroup("Physics")]
+    [Export] public float PushForce { get; set; } = 5.0f;
 }
