b62543bc734b4b55aee0356a2f776571b1145795
Erickian2007 <erickian2007@gmail.com>
2026-06-23 13:38:28 -0300

    Add PawnState resource: runtime action/weapon blackboard for entities

---
+++ content/source/resources/PawnState.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+namespace RPGFramework.Resources;
+
+// Blackboard de estado runtime do Pawn. Vários sistemas leem/escrevem:
+// animação escolhe moveset, input bloqueia ações, hitbox checa se pode acertar, etc.
+[GlobalClass]
+public partial class PawnState : Resource
+{
+    public enum Weapon { Unarmed, Sword }
+    public enum Action { None, Attacking, Blocking, Dodging, Hurt, Dead }
+
+    // Emitido sempre que arma ou ação mudam (Resource.Changed é reservado, por isso nome próprio).
+    [Signal] public delegate void StateChangedEventHandler();
+
+    [Export] public Weapon CurrentWeapon { get; set; } = Weapon.Unarmed;
+    [Export] public Action CurrentAction { get; set; } = Action.None;
+
+    public bool IsArmed => CurrentWeapon != Weapon.Unarmed;
+    public bool IsBusy  => CurrentAction != Action.None;
+    public bool IsDead  => CurrentAction == Action.Dead;
+
+    public void SetWeapon(Weapon weapon)
+    {
+        if (CurrentWeapon == weapon) return;
+        CurrentWeapon = weapon;
+        EmitSignal(SignalName.StateChanged);
+    }
+
+    public void SetAction(Action action)
+    {
+        if (CurrentAction == action) return;
+        CurrentAction = action;
+        EmitSignal(SignalName.StateChanged);
+    }
+}
