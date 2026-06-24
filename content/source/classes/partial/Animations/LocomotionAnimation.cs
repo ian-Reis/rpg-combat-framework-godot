0f86cb075c719869c4f377a5cd064c1979d2aa87
Erickian2007 <erickian2007@gmail.com>
2026-06-24 19:17:47 -0300

    LocomotionAnimation: use BlendSpace1DPosition dictionary, add Init()

--- content/source/classes/partial/Animations/LocomotionAnimation.cs
+++ content/source/classes/partial/Animations/LocomotionAnimation.cs
@@ -4,10 +4,17 @@ namespace RPGFramework.Core;
 
 public partial class DefaultControllerAnimation
 {
+    private void Init()
+    {
+        _movementPB?.Travel("Locomotion");
+    }
+
     private void UpdateLocomotion(float horizontalSpeed)
     {
         float maxSpeed = Stats?.Speed ?? 5f;
         float blendValue = Mathf.Clamp(horizontalSpeed / maxSpeed, 0f, 1f);
-        AnimTree.Set(LocomotionBlendParam, blendValue);
+
+        if (BlendSpace1DPosition.TryGetValue("locomotion", out var path))
+            AnimTree.Set(path, blendValue);
     }
 }
