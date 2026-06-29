bdf31dfc252ae53c6ce41e544c9ec90f76dcf4c0
Erickian2007 <erickian2007@gmail.com>
2026-06-29 19:42:38 -0300

    implement slide animation with one-shot fire/fade-out and exit travel

--- content/source/classes/partial/Animations/SlideAnimation.cs
+++ content/source/classes/partial/Animations/SlideAnimation.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace RPGFramework.Core;
@@ -6,4 +7,33 @@ public partial class AnimationController
 {
     [ExportGroup("Slide")]
     [Export] public float SlideBlendSpeed { get; set; } = 10f;
+    [Export] public StringName AnimNameEnd { get; set; } = "Slide_Exit";
+
+    private void SlideCallbacks()
+    {
+        OnAnimFinished(AnimNameEnd, () =>
+        {
+            if (Parameters.OneShotRequest.TryGetValue("slide", out var slideOSPath))
+                AnimTree.Set(slideOSPath, (int)AnimationNodeOneShot.OneShotRequest.FadeOut);
+                
+                Pawn.State.IsSliding = false;
+        });
+    }
+
+    private void UpdateSlide(float dt, float horizontalSpeed)
+    {
+        float target = Mathf.Clamp(horizontalSpeed, 0, 1);
+        if (target >= 1 && Input.IsActionPressed("slide") && !Pawn.State.IsSliding)
+        {
+            if (Parameters.OneShotRequest.TryGetValue("slide", out var slideOSPath))
+                AnimTree.Set(slideOSPath, (int)AnimationNodeOneShot.OneShotRequest.Fire);
+
+            Pawn.State.IsSliding = true;
+        }
+        else if (!Input.IsActionPressed("slide") && Pawn.State.IsSliding)
+        {
+            
+            _slidePB?.Travel(AnimNameEnd);
+        }
+    }
 }
