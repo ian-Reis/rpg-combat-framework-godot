8bff0a0289345ec338b2154a0f1c3bec584d350e
Erickian2007 <erickian2007@gmail.com>
2026-06-30 16:14:50 -0300

    feat: add Pawn3D export ref and use Pawn.GetHorizontalSpeed() for rotation direction

--- content/source/classes/partial/RotateDirection3D.cs
+++ content/source/classes/partial/RotateDirection3D.cs
@@ -1,10 +1,12 @@
 using Godot;
+using RPGFramework.Entitys;
 
 namespace RPGFramework.Core;
 
 [GlobalClass]
 public partial class RotateDirection3D : Node3D
 {
+    [Export] public Pawn3D Pawn { get; set; }
     [Export] public SpringArm3D SpringArm { get; set; }
     [Export] public float Speed { get; set; } = 10f;
 
@@ -19,7 +21,8 @@ public partial class RotateDirection3D : Node3D
         if (SpringArm == null || !RotationEnabled)
             return;
 
-        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+        Vector2 inputDir = IsInstanceValid(Pawn) ? Pawn.GetHorizontalSpeed() : Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+        
         if (inputDir == Vector2.Zero)
             return;
 
