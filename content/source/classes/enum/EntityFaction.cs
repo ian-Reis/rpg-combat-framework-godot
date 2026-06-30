4ad9609b8a3af45811215c059de8a735b8f33912
Erickian2007 <erickian2007@gmail.com>
2026-06-30 16:26:33 -0300

    fix: rename namespace RPGFramework.Entitys to RPGFramework.Entities

--- content/source/classes/enum/EntityFaction.cs
+++ content/source/classes/enum/EntityFaction.cs
@@ -1,4 +1,4 @@
-namespace RPGFramework.Entitys;
+namespace RPGFramework.Entities;
 
 // Tipo/facção da entidade. Organiza Player, inimigos, NPCs e (futuro) pets.
 // Cada Pawn se registra num grupo pela facção (ex: "enemy"), e a IA mira alvos por facção.
