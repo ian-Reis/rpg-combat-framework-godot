e4609e9d3114ae0c7ce06620b491eba50a234c80
Erickian2007 <erickian2007@gmail.com>
2026-07-02 17:42:39 -0300

    Add DestructiveObject3D module with scene and script

---
+++ content/source/classes/partial/DestructiveObject3D/DestructiveObject3D.cs
@@ -0,0 +1,40 @@
+using Godot;
+using RPGFramework.Helpers;
+
+namespace RPGFramework.Core;
+
+[GlobalClass]
+public partial class DestructiveObject3D : Node3D
+{
+    [ExportGroup("References")]
+    [Export] public HurtBoxArea3D HurtBoxArea3D;
+    [Export] public HealthComponent HealthComponent;
+    [Export] public AnimationPlayer AnimationPlayer;
+
+    private StringName _myGroup = "destructive";
+
+    public override void _Ready()
+    {
+        RegisterGroup.Add(this, _myGroup);
+
+
+        // Connect Signals
+        HealthComponent.HealthChanged += Hit;
+        HealthComponent.Died += Destroy;
+    }
+
+    public virtual void Hit(float current, float max)
+    {
+        GD.Print("          lasjkdafslfslfsdfajkl");
+        // default example
+        if (IsInstanceValid(AnimationPlayer))
+        {
+            if (AnimationPlayer.HasAnimation("hit")) 
+                AnimationPlayer.Play("hit");
+        }
+
+    }
+
+    public virtual void Destroy() => QueueFree();
+
+}
