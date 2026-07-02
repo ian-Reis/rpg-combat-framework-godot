5c644e669cbb68959087894a7bd76d84fc716edb
Erickian2007 <erickian2007@gmail.com>
2026-07-02 17:42:17 -0300

    Implement slide velocity with directional control

--- content/source/classes/partial/Pawn3D/Slide.cs
+++ content/source/classes/partial/Pawn3D/Slide.cs
@@ -8,6 +8,19 @@ public partial class Pawn3D
     private void Slide(ref Vector3 velocity, float dt)
     {
         State.IsSliding = State?.CanSlide is true && IsOnFloor() && (GetHorizontalSpeed().Length() > 0.8f) && ReadSlide() && State.CurrentAction != PawnState.Action.Attacking;
+        if (State.IsSliding)
+        {
+            if (_slideVelocity == Vector3.Zero)
+                _slideVelocity = velocity;
+            
+            Vector3 target = State.IsSliding ? _slideVelocity + ReadMoveDirection() : Vector3.Zero;
+            
+            velocity = target;
+        }
+        else
+        {
+            _slideVelocity = Vector3.Zero;
+        }
     }
 
 }
